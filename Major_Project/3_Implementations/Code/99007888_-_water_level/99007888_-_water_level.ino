83184feb886a33f386000f5cfc975110433d886e
AKIVISHNU <98825305+AKIVISHNU473@users.noreply.github.com>
2022-03-08 15:44:24 +0530

    Add files via upload

---
+++ Major_Project/3_Implementations/Code/99007888_-_water_level/99007888_-_water_level.ino
@@ -0,0 +1,53 @@
+#include <NewPing.h>
+#include <Servo.h>
+#define triPin 12
+#define echoPin 13
+#define MAXIMUM_DISTANCE 100
+NewPing sonar(triPin, echoPin, MAXIMUM_DISTANCE);
+int LED2= 3, LED1 = 2;
+Servo myservo;
+
+void setup() {
+  
+  
+  Serial.begin (115200);
+    pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(triPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+ 
+  myservo.attach(9);
+
+}
+
+void loop() {
+  
+int Threshold,posisi=0,i;
+int duration;
+  digitalWrite(triPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(triPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triPin, HIGH);
+  duration = pulseIn(echoPin, HIGH);
+  Threshold = (duration/2)/29.1;
+    Serial.print(Threshold);
+    Serial.println(" cm");
+
+    //condition for output
+if(Threshold<= 6)
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    myservo.write(180);
+}
+  else{
+    digitalWrite(LED2, LOW);
+      digitalWrite(LED1, HIGH);
+      myservo.write(90);
+  }
+  delay(200);
+
+  
+
+}
